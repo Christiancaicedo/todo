540cd2d4a63cd04fb9430de8f0067dea66589d46
Christiancaicedo <christian.caicedo96@gmail.com>
2022-10-05 21:49:41 -0500

    re-wrote todo list code

--- todo.py
+++ todo.py
@@ -1,14 +1,15 @@
+from audioop import add
+
+
 todo_list = []
 while True:
     print("What do you need to do?")
     add_todo = input("")
     todo_list.append(add_todo)
     for item in todo_list:
-        if item == f"{add_todo} completed":
-            del item
-        else:
-            convert_to_string = ""
-            for i in todo_list:
-                convert_to_string += i
+        print(item)
+    print()
 
-    print(convert_to_string)
+    if add_todo == f"{add_todo} completed":
+        del add_todo
+        print(todo_list)
