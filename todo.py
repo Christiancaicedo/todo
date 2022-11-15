40300c7165b794b23cf0c3d78736417aa07283b6
Christiancaicedo <christian.caicedo96@gmail.com>
2022-11-15 09:01:58 -0600

    completed add and remove functions for todo list

--- todo.py
+++ todo.py
@@ -1,5 +1,6 @@
 todo_list = []
 
+
 def add_to_list(list_item: str) -> list:
     """Function adds item to the to-do list
 
@@ -9,11 +10,16 @@ def add_to_list(list_item: str) -> list:
     Returns:
         list: Appends to-do list to add item to be completed
     """
-    todo_list.append(list_item.capitalize())
+    strip_item = list_item.lstrip(" ").capitalize()
+    todo_list.append(strip_item)
+    print()
+    print(f"{strip_item} has been added to your to-do list")
+    print("Here is your updated list:")
     for index, item in enumerate(todo_list):
             print(f"{index+1}. {item}")
-            
+    print()
 
+            
 def remove_from_list(item_to_remove: str) -> list:
     """Removes item from list
 
@@ -23,21 +29,33 @@ def remove_from_list(item_to_remove: str) -> list:
     Returns:
         list: Updated to-do list with requested item removed
     """
-    if item_to_remove.casefold() in todo_list:
-        todo_list.remove(item_to_remove)
-        print(f"{item_to_remove} has been removed from your to-do list")
-        print(enumerate(todo_list))
+    strip_item = item_to_remove.lstrip(" ").capitalize()
+    if strip_item in todo_list:
+        todo_list.remove(strip_item)
+        print()
+        print(f"{strip_item} has been removed from your to-do list")
+        print("Here is your updated list:")
+        for index, item in enumerate(todo_list):
+            print(f"{index+1}. {item}")
         print()
 
 
+t_list = "That task is not on the list"
+
+# For loop that requests a users input to add or remove task to list.
+# "add:..." will add an item to the list "remove:..." will remove an
+# item from the list
 while True:
     user_input = input("What do you need to get done?\n")
     if user_input.casefold() == 'close':
         break
-    elif 'remove' in user_input.casefold():
-        remove_from_list(user_input[7:])
+    
+    command, task = user_input.casefold().split(':')
+    if command == 'add':
+        add_to_list(task.casefold())
+    elif command == 'remove':
+        remove_from_list(task.casefold())
     else:
-        add_to_list(user_input)
-        # todo_list.append(user_input.capitalize())
-        # for index, item in enumerate(todo_list):
-        #     print(f"{index+1}. {item}")
+        print()
+        print(f"{t_list}_^10")
+        print()
